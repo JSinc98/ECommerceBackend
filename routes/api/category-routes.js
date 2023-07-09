5f99485a3537a9dcff85f2c204de6f1cedd7c7b1
Joshua Sinclair <josh.r.sinclair@gmail.com>
2023-07-09 11:14:17 -0400

    pushing up to repo

---
+++ routes/api/category-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  const response = await Category.findAll({
+    include: [{model: Product}],
+  });
+  return res.json(response);
+});
+
+router.get('/:id', async (req, res) => {
+  const response = await Category.findByPk(req.params.id, {
+    include: [{model: Product}],
+  });
+  return res.json(response);
+});
+
+router.post('/', async (req, res) => {
+  const response = await Category.create(req.body);
+  return res.json(response);
+});
+
+router.put('/:id', async (req, res) => {
+  const response = await Category.update(
+    {
+      category_name: req.body.category_name,
+    },
+    {
+      where: {
+        category_id: req.params.id,
+      },
+    }
+  );
+  return res.json(`Updated`);
+});
+
+router.delete('/:id', async (req, res) => {
+  const response = await Category.destroy({
+    where: {
+      category_id: req.params.id,
+    },
+  });
+  return res.json(`Deleted`);
+});
+
+module.exports = router;
---
+++ routes/api/tag-routes.js
@@ -0,0 +1,48 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+router.get('/', async (req, res) => {
+  const response = await Tag.findAll({
+    include: [{model: Product}],
+  });
+  return res.json(response);
+});
+
+router.get('/:id', async (req, res) => {
+  const response = await Tag.findByPk(req.params.id, {
+    include: [{model: Product}],
+  });
+  return res.json(response);
+});
+
+router.post('/', async (req, res) => {
+  const response = await Tag.create(req.body);
+  return res.json(response);
+});
+
+router.put('/:id', async (req, res) => {
+  const response = await Tag.update(
+    {
+      tag_name: req.body.tag_name,
+    },
+    {
+      where: {
+        tag_id: req.params.id,
+      },
+    }
+  );
+  return res.json(response);
+});
+
+router.delete('/:id', async (req, res) => {
+  const response = await Tag.destroy({
+    where: {
+      tag_id: req.params.id,
+    },
+  });
+  return res.json(response);
+});
+
+module.exports = router;
